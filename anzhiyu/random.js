22cdb679a1425600e20e26a4b980c9d8871f53c6
Mako800 <527650607@qq.com>
2026-03-15 22:28:01 +0800

    Site updated: 2026-03-15 22:28:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/15/算法笔记：模拟退火/","2026/03/15/hello-world/"];function toRandomPost(){
+var posts=["2026/03/15/hello-world/","2026/03/15/算法笔记：模拟退火/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
