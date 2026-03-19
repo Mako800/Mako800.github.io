a72ce93fe3f9b5c5551fa4d535f9da7bca77e816
Mako800 <527650607@qq.com>
2026-03-19 21:49:25 +0800

    Site updated: 2026-03-19 21:49:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/18/NOI2023春赛游寄——亦回望我的OI生涯/","2026/03/16/算法笔记：树上背包/","2026/03/15/算法笔记：模拟退火/"];function toRandomPost(){
+var posts=["2026/03/16/算法笔记：树上背包/","2026/03/15/算法笔记：模拟退火/","2026/03/18/NOI2023春赛游寄——亦回望我的OI生涯/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
